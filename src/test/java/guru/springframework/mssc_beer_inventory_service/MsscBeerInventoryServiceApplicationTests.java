b8991dbe719c21d2845be3f129f349300d645943
damned dementor <damned.dementor@gmail.com>
2025-01-14 12:01:20 +0300

    initial commit

---
+++ src/test/java/guru/springframework/mssc_beer_inventory_service/MsscBeerInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.mssc_beer_inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
